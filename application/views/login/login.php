18f62e2d39bd1a5866a7539179d28972def44436
Dstar18 <dedybintang18@gmail.com>
2021-09-02 14:58:37 +0700

    first commit

---
+++ application/views/login/login.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Admin</title>
+    </head>
+    <body>
+        <h3>INI HALAMAN ADMIN COMPANY PROFILE</h3>
+    </body>
+</html>
---
+++ application/views/website/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Website</title>
+    </head>
+    <body>
+        <h3>INI HALAMAN WEBSITE COMPANY PROFILE</h3>
+    </body>
+</html>
